d54cf0c0cbe9e2260dbfdc1261146d0441742b0f
zhouzhou <2821511460@qq.com>
2023-10-12 21:24:01 +0800

    10/12

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,7 +13,6 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
 // fn calculate_price_of_apples {
@@ -26,8 +25,12 @@ fn verify_test() {
     let price3 = calculate_price_of_apples(41);
     let price4 = calculate_price_of_apples(65);
 
-    assert_eq!(70, price1);
-    assert_eq!(80, price2);
+    assert_eq!(35, price1);
+    assert_eq!(40, price2);
     assert_eq!(41, price3);
     assert_eq!(65, price4);
+}
+fn main()
+{
+    
 }
